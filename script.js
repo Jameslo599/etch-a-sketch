994c9370ffe8b4e3fca991e7fe838b5874572de0
Jameslo599 <jameslo599@gmail.com>
2020-12-01 02:26:48 -0600

    added 'clear' button to reset the grid and also allow user to decide how many squares per side

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 //Generates a 16x16 grid upon loading of webpage
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+function makeRows(rows) {
   container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.setProperty('--grid-cols', rows);
+  for (c = 0; c < (rows * rows); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "gridItem";
   };
   hoverColor();
 };
 
-makeRows(16, 16);
+makeRows(16);
 
 //Changes color of grid divs when mouse hovers over them
 function changeColor() {
     ;
 }
 function hoverColor() {
-    let boxes = document.querySelectorAll(".gridItem");
-    boxes.forEach(boxes => {
-        boxes.addEventListener("mouseover", () => {
-            boxes.style.backgroundColor = "black"})
-        });
+  let boxes = document.querySelectorAll(".gridItem");
+  boxes.forEach(boxes => {
+    boxes.addEventListener("mouseover", () => {
+        boxes.style.backgroundColor = "black"})
+      });
 }
+
+//Remove grid before reset
+function removeGrid() {
+  let cell = document.querySelectorAll(".gridItem");
+  cell.forEach(cell => {
+    container.removeChild(cell);
+  })
+}
+//Resets the grid
+function eraseGrid() {
+  let boxes = document.querySelectorAll(".gridItem");
+  boxes.forEach(boxes => {
+    boxes.style.backgroundColor = "white";
+  })
+  removeGrid();
+  let rows = prompt("Enter number of squares per side",);
+  return makeRows(rows);
+}
+clearButton = document.getElementById("eraseButton");
+clearButton.addEventListener("click", eraseGrid);
