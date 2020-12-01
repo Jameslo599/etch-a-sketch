09aa68885901686b88726b80836b92d0415cc568
Jameslo599 <jameslo599@gmail.com>
2020-12-01 00:00:45 -0600

    grid items turn black when hovered over

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Generate a 16x16 grid upon loading of webpage
+//Generates a 16x16 grid upon loading of webpage
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
@@ -6,10 +6,21 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "gridItem";
   };
+  hoverColor();
 };
 
 makeRows(16, 16);
 
 //Changes color of grid divs when mouse hovers over them
+function changeColor() {
+    ;
+}
+function hoverColor() {
+    let boxes = document.querySelectorAll(".gridItem");
+    boxes.forEach(boxes => {
+        boxes.addEventListener("mouseover", () => {
+            boxes.style.backgroundColor = "black"})
+        });
+}
